96bd0b351f7bf9e57f3a1b6a11b0d56d77a41f33
Joon Jang <w.joon.jang@gmail.com>
2020-12-11 14:12:27 -0800

    deleted weather example added PWA requirements, npm doesnt work, may be client side error or project error after a --force command

--- DailyActivityApp/ClientApp/src/app/app.component.ts
+++ DailyActivityApp/ClientApp/src/app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { ConnectionService } from 'connection-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  status = 'ONLINE';
+  isConnected = true;
+
+  constructor(private connectionService: ConnectionService) {
+    this.connectionService.monitor().subscribe(isConnected => {
+      this.isConnected = isConnected;
+      if (this.isConnected) {
+        this.status = "ONLINE";
+      }
+      else {
+        this.status = "OFFLINE";
+      }
+    })
+  }
 }
