6819175be5c751f139ea81e98ff103fcca1ffd16
Joon Jang <w.joon.jang@gmail.com>
2020-12-14 13:40:08 -0800

    - able to see bar chart in tracking now

--- DailyActivityApp/ClientApp/src/app/app.module.ts
+++ DailyActivityApp/ClientApp/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { ChartsModule, ThemeService } from 'ng2-charts';
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -22,13 +24,15 @@ import { TrackingComponent } from './tracking/tracking.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ChartsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'tracking', component: TrackingComponent },
-    ])
+    ]),
+    
   ],
-  providers: [],
+  providers: [ThemeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
